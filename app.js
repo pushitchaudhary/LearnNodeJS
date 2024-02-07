eafe2bf9ff7b798c5131cee3a7afd6e354d9e9ac
pushitchaudhary <pushitcoc@gmail.com>
2024-02-07 19:32:10 +0545

    first add

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('this is home page');
+})
+
+app.get('/about',(req,res)=>{
+    res.send('this is about page heelo');
+})
+
+app.listen(3000,()=>{
+    console.log('node js has started at 3000')
+})
